2980c72c07ea7495cbe3eb17a93cfa556edc4166
esmali <maryam.e.falak@gmail.com>
2019-02-17 18:02:10 +0000

    location field added in job class

---
+++ db/migrate/20190217180135_add_fiel_to_job.rb
@@ -0,0 +1,5 @@
+class AddFielToJob < ActiveRecord::Migration[5.2]
+  def change
+    add_column :jobs, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_13_192841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,10 +24,11 @@ ActiveRecord::Schema.define(version: 2019_02_13_192841) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.integer "effort"
-    t.integer "job_id"
+    t.bigint "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_tasks_on_job_id"
   end
 
+  add_foreign_key "tasks", "jobs"
 end
