0026788bc2a6b42f54098751433ec6bf837aeee9
esmali <maryam.e.falak@gmail.com>
2019-02-17 18:16:28 +0000

    Issue 8 resolve

---
+++ db/migrate/20190217181415_add_fie_to_job.rb
@@ -0,0 +1,5 @@
+class AddFieToJob < ActiveRecord::Migration[5.2]
+  def change
+    add_column :jobs, :status, :boolean ,default:false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_180801) do
+ActiveRecord::Schema.define(version: 2019_02_17_181415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_02_17_180801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.boolean "status", default: false
   end
 
   create_table "tasks", force: :cascade do |t|
