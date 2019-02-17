440932f76c4dcdc21a193447c4184c4457e34d7c
esmali <maryam.e.falak@gmail.com>
2019-02-17 18:07:34 +0000

    issue 3 solve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_192841) do
+ActiveRecord::Schema.define(version: 2019_02_17_180135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_192841) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "tasks", force: :cascade do |t|
